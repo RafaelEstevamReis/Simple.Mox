102b8e13636cbf9d67d0696f0589330a9b79ecdd
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2024-01-27 02:41:38 -0300

    Add GetStatistics

---
+++ Simple.Mox/Models/NodeRRD.cs
@@ -0,0 +1,52 @@
+﻿namespace Simple.Mox.Models;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+
+
+public record NodeRRD
+{
+    public enum TimeFrame
+    {
+        hour,
+        day,
+        week,
+        month,
+        year
+    }
+    public enum DataSet
+    {
+        loadavg,
+        maxcpu,
+        cpu,
+        iowait,
+        memtotal,
+        memused,
+        swaptotal,
+        swapused,
+        roottotal,
+        rootused,
+        netin,
+        netout,
+    }
+
+    public int maxcpu { get; set; }
+    public long swaptotal { get; set; }
+    public int swapused { get; set; }
+    public float cpu { get; set; }
+    public float netout { get; set; }
+    public float netin { get; set; }
+    [JsonConverter(typeof(UnixDateTimeConverter))]
+    public DateTime time { get; set; }
+    public long memtotal { get; set; }
+    public long roottotal { get; set; }
+    public float rootused { get; set; }
+    public long memused { get; set; }
+    public float iowait { get; set; }
+    public float loadavg { get; set; }
+}
+public record NodeRRD_StringImage
+{
+    public string Image { get; set; }
+}
--- Simple.Mox/Sources/Node.cs
+++ Simple.Mox/Sources/Node.cs
@@ -2,10 +2,12 @@
 
 using Simple.Mox.Models;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 public class Node
 {
+    private static readonly Encoding ImageEncoding = Encoding.GetEncoding("iso-8859-1");
     public Instance Instance { get; }
     public string NodeName { get; }
 
@@ -25,6 +27,19 @@ public class Node
     private async Task<API.Response<T>> post<T>(string service, object? value)
     {
         return await Instance.api.PostAsync<T>($"/api2/json/nodes/{NodeName}/{service}", value);
+    }
+
+    public async Task<NodeRRD[]?> GetStatistics(NodeRRD.TimeFrame timeFrame)
+    {
+        var r = await get<ResponseData<NodeRRD[]>>($"rrddata?timeframe={timeFrame}");
+        r.EnsureSuccessStatusCode();
+        return r.Data.Data;
+    }
+    public async Task<byte[]> GetStatisticsImage(NodeRRD.TimeFrame timeFrame, NodeRRD.DataSet dataset)
+    {
+        var r = await get<ResponseData<NodeRRD_StringImage>>($"rrd?timeframe={timeFrame}&ds={dataset}");
+        r.EnsureSuccessStatusCode();
+        return ImageEncoding.GetBytes(r.Data.Data.Image);
     }
 
     public async Task<NodeStatus?> GetStatus()
