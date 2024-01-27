33f6e043f32e8e5fe9c2eef591e56ed091d48b73
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2024-01-27 00:57:02 -0300

    First Commit

---
+++ Simple.Mox/AuthParams.cs
@@ -0,0 +1,14 @@
+﻿namespace Simple.Mox;
+
+public class AuthParams
+{
+    public string User { get; set; } = string.Empty;
+    public string Realm { get; set; } = "pam";
+    public string TokenName { get; set; } = string.Empty;
+    public string TokenSecret { get; set; } = string.Empty;
+
+    public bool AllowInsecureCertificates { get; set; } = false;
+
+    public string GetApiToken()
+        => $"{User}@{Realm}!{TokenName}={TokenSecret}";
+}
---
+++ Simple.Mox/Client.cs
@@ -0,0 +1,33 @@
+﻿using Simple.API;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Simple.Mox;
+
+public class Client
+{
+    private readonly ClientInfo api;
+    private readonly AuthParams auth;
+
+    public Client(string url, AuthParams auth)
+    {
+        this.auth = auth;
+
+        var handler = new HttpClientHandler();
+        handler.ServerCertificateCustomValidationCallback ??= certificateValidation;
+        api = new ClientInfo(url, handler);
+
+        api.SetAuthorization($"PVEAPIToken={auth.GetApiToken()}");
+    }
+    private bool certificateValidation(HttpRequestMessage message,
+                                           System.Security.Cryptography.X509Certificates.X509Certificate2? certificate,
+                                           System.Security.Cryptography.X509Certificates.X509Chain? chain,
+                                           System.Net.Security.SslPolicyErrors policy)
+    {
+        if (auth.AllowInsecureCertificates) return true;
+
+        return policy == System.Net.Security.SslPolicyErrors.None;
+    }
+
+
+}
