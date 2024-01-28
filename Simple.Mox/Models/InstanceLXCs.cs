671b665edf0fb427d270166ac4875a0bee416513
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2024-01-28 19:19:11 -0300

    Update InstanceLXCs.cs

--- Simple.Mox/Models/InstanceLXCs.cs
+++ Simple.Mox/Models/InstanceLXCs.cs
@@ -7,4 +7,5 @@ public record InstanceLXCs : InstanceItem
     public string type { get; set; }
     public long swap { get; set; }
     public long maxswap { get; set; }
+    public float fswap =>  swap / (float)maxswap;
 }
